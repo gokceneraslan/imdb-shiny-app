e9ca8a0958f223ac4c347c6fbff7ea2c6d3648e1
Gökçen Eraslan <gokcen.eraslan@gmail.com>
2014-01-05 00:22:09 +0200

    Introduce tabPanels

--- server.r
+++ server.r
@@ -8,7 +8,8 @@ library(stringr)
 library(shinyIncubator)
 
 plot.Genre.Year <- function(df) {
-  ngenres <- 6 #get most frequent 6 genres and mark the rest as "Other"
+  df <- copy(df)
+  ngenres <- 7 #get most frequent 7 genres and mark the rest as "Other"
   top.genres <- count(df, 'Genre')
   top.genres <- top.genres[order(top.genres$freq, decreasing=T),][1:ngenres, 1]
   df[, Genre:=ifelse(Genre %in% top.genres, Genre, 'Other')]
@@ -16,10 +17,14 @@ plot.Genre.Year <- function(df) {
   genre.counts <- na.omit(ddply(df, c('Year', 'Genre'), summarise, Count=length(Genre)))
   genre.counts <- melt(dcast(genre.counts, ...~Genre, fill=0), id='Year', value.name='Count', variable.name='Genre') #include counts of ALL genres to make nPlot happy
   
-#   genre.plot <- nPlot(Count ~ Year, group =  'Genre', data = genre.counts, type = 'stackedAreaChart', id = 'chart')
-  genre.plot <- nPlot(Count ~ Year, group =  'Genre', data = genre.counts, type = 'lineWithFocusChart', id = 'chart')
-#   genre.plot$chart(useInteractiveGuideline=TRUE)
+  genre.plot <- nPlot(Count ~ Year, group =  'Genre', data = genre.counts, type = 'stackedAreaChart', id = 'chart')
+#   genre.plot <- nPlot(Count ~ Year, group =  'Genre', data = genre.counts, type = 'lineWithFocusChart', id = 'chart')
+
+  #genre.plot$chart(useInteractiveGuideline=TRUE)
   #print(genre.plot)
+
+   genre.plot$chart(showControls=T)
+
   return(genre.plot)
 }
 
@@ -49,79 +54,41 @@ shinyServer(function(input, output, session) {
                   'Great Depression' = 'DEP',
                   'Vietnam War' = 'VIETNAM',
                   '9/11' = 'SEP11')                     
-    if (str_trim(input$keywords) == '') {
-      return(imdb[imdb[[var]] == T][1:1000]) 
-    }
-    else {
-      regex <- keywords.to.regex(input$keywords)
-      print(regex)
-      system.time(imdb[, USER:=grepl(regex, Plot, ignore.case=T, perl=T), by=Key])
-      print(regex)
-      return(imdb[imdb$USER == T][1:1000])  #limit results by first 1000 movies
-    }
+#     if (str_trim(input$keywords) == '') {
+       return(imdb[imdb[[var]] == T][1:1000]) 
+#     }
+#     else {
+#       regex <- keywords.to.regex(input$keywords)
+#       print(regex)
+#       system.time(imdb[, USER:=grepl(regex, Plot, ignore.case=T, perl=T), by=Key])
+#       print(regex)
+#       return(imdb[imdb$USER == T][1:1000])  #limit results by first 1000 movies
+#     }
   })
-  
-  output$trendChart <- renderChart({
-    
+
+  generate.table <- function() {
     withProgress(session, min=1, max=15, expr={
       
       setProgress(message = 'Calculation in progress',
-                  detail = 'This may take a while...')
-      dd <- df()
-      
-      if (nrow(dd) == 0) {
-        return(NULL)
-      }
-      n1 <<- plot.Genre.Year(dd)
-      n1$set(dom = "trendChart")
-      
-      #     setProgress(message = 'Calculation in progress',
-      #                 detail = 'This may take a while...',
-      #                 value=15)
-      return(n1)
-      
-    })
-  })
-  
-  callback = "#! 
-      function (nRow) {
-            $('td', nRow).each(function (index) {
-                var maxChars = 80;
-                    var unfilteredText = $(this).text();
-                    if (unfilteredText.length > maxChars && maxChars > 3) {
-                        $(this).attr('title', unfilteredText);
-                        $(this).html(unfilteredText.substring(0, maxChars-4) + '...');
-                    }
-            });
-            return nRow;
-        } !#"
-  
-  output$trendTable <- renderChart2({
-#   output$trendTable <- renderDataTable({
-      
-    withProgress(session, min=1, max=15, expr={
-      
-      setProgress(message = 'Calculation in progress',
-                  detail = 'This may take a while...')
+                  detail = 'This may take a while...', value=0)
       
       gt <- df()
       gt <- gt[!is.na(gt$Title)] #remove entries without titles
       gt <- gt[order(gt$NormRating, decreasing=T)] #sort movies by normalized ratings
-      print(str(gt))
+#       print(str(gt))
       gt <- gt[, list(Title, Year, Genre, Category, Language, NormRating,Plot)] #only include meaningful col.s
-      
       callback = "#! 
-      function (nRow) {
-            $('td', nRow).each(function (index) {
-                var maxChars = 80;
-                    var unfilteredText = $(this).text();
-                    if (unfilteredText.length > maxChars && maxChars > 3) {
-                        $(this).attr('title', unfilteredText);
-                        $(this).html(unfilteredText.substring(0, maxChars-4) + '...');
-                    }
-            });
-            return nRow;
-        } !#"
+        function (nRow) {
+              $('td', nRow).each(function (index) {
+                  var maxChars = 80;
+                      var unfilteredText = $(this).text();
+                      if (unfilteredText.length > maxChars && maxChars > 3) {
+                          $(this).attr('title', unfilteredText);
+                          $(this).html(unfilteredText.substring(0, maxChars-4) + '...');
+                      }
+              });
+              return nRow;
+          } !#"
       
       gt <- dTable(gt, aaSorting = list(c(5, "desc")), sPaginationType="bootstrap",
                    fnRowCallback=callback)
@@ -132,21 +99,73 @@ shinyServer(function(input, output, session) {
       
       return(gt)
     })
+    
+  }
+
+  generate.plot <- function(id) {
+    withProgress(session, min=1, max=15, expr={
+      
+      setProgress(message = 'Calculation in progress',
+                  detail = 'This may take a while...', value=0)
+      dd <- df()
+      
+      if (nrow(dd) == 0) {
+        return(NULL)
+      }
+      n1 <<- plot.Genre.Year(dd)
+      n1$set(dom=id)
+      
+      #     setProgress(message = 'Calculation in progress',
+      #                 detail = 'This may take a while...',
+      #                 value=15)
+      return(n1)
+      
+    }) 
+  }
+
+  output$trendGD <- renderChart({
+    generate.plot(id='trendGD')
+  })
+
+  output$trendWW2 <- renderChart({
+    generate.plot(id='trendWW2')
+  })
+  
+  output$trendVW <- renderChart({
+    generate.plot(id='trendVW')
+  })
+  
+  output$trend911 <- renderChart({
+    generate.plot(id='trend911')
+  })
+
+  output$trendTable <- renderChart2({
+    generate.table()
+  })
+  
+  output$trendTable2 <- renderChart2({
+    generate.table()
+  })
+  
+  output$trendTable3 <- renderChart2({
+    generate.table()
+  })
+  
+  output$trendTable4 <- renderChart2({
+    generate.table()
   })
 
   output$wikipedia <- renderUI({
-    if (str_trim(input$keywords) == '') {
       URI <- switch(input$event, 
                     'World War 2' = 'http://en.m.wikipedia.org/wiki/World_war_2',
                     'Great Depression' = 'http://en.m.wikipedia.org/wiki/Great_Depression',
                     'Vietnam War' = 'http://en.m.wikipedia.org/wiki/Vietnam_war',
                     '9/11' = 'http://en.m.wikipedia.org/wiki/September_11_attacks')
-    }
-    else {
-      wpkeywords <- keywords.to.wpsearch(input$keywords)
-      URI <- paste0('http://en.m.wikipedia.org/w/index.php?search=', wpkeywords)
-    }
-    tags$iframe(src=URI, width=400, height=1000, tags$p('Your browser does not support iframes.'))
+#     }
+#     else {
+#       wpkeywords <- keywords.to.wpsearch(input$keywords)
+#       URI <- paste0('http://en.m.wikipedia.org/w/index.php?search=', wpkeywords)
+#     }
+    tags$iframe(src=URI, width=400, height=1100, tags$p('Your browser does not support iframes.'))
   })
-
 })
--- ui.r
+++ ui.r
@@ -7,20 +7,23 @@ shinyUI(pageWithSidebar(
   headerPanel("IMDB Trends"),
   
   sidebarPanel(
-    selectInput(inputId = "event",
-                label = h4("Choose an Event"),
-                choices = c('World War 2', 'Great Depression', 'Vietnam War', '9/11'),
-                selected = "World War 2"),
-    h4(' or '),
-    textInput("keywords", h4("Search for keywords:"), value = ""),    
-    submitButton("Search"),
-    br(),
+#     selectInput(inputId = "event",
+#                 label = h4("Choose an Event"),
+#                 choices = c('World War 2', 'Great Depression', 'Vietnam War', '9/11'),
+#                 selected = "World War 2"),
+#     h4(' or '),
+#     textInput("keywords", h4("Search for keywords:"), value = ""),    
+#     submitButton("Search"),
+#     br(),
     htmlOutput('wikipedia')
   ),
   mainPanel(
     progressInit(),
-    showOutput("trendChart", 'nvd3'),
-    chartOutput("trendTable", 'datatables')
-    #dataTableOutput("trendTable")
+    tabsetPanel(
+      tabPanel("Great Depression", showOutput("trendGD", 'nvd3'), chartOutput("trendTable", 'datatables'), value='Great Depression'),
+      tabPanel("World War 2", showOutput("trendWW2", 'nvd3'), chartOutput("trendTable2", 'datatables'), value='World War 2'),
+      tabPanel("Vietnam War", showOutput("trendVW", 'nvd3'), chartOutput("trendTable3", 'datatables'), value='Vietnam War'),
+      tabPanel("9/11 Attacks", showOutput("trend911", 'nvd3'), chartOutput("trendTable4", 'datatables'), value='9/11'),
+      id='event')
   )
 ))
