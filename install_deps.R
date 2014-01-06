973f279ab7d3d0db66ec155669182fce4261c3ba
Gökçen Eraslan <gokcen.eraslan@gmail.com>
2014-01-06 20:09:10 +0000

    Add an dependency installer script.

---
+++ install_deps.R
@@ -0,0 +1,11 @@
+install.packages(c('devtools', 'stringr'))
+
+library(devtools)
+install_github('hadley/plyr')
+install.packages('reshape2')
+
+install.packages('data.table', repos='http://R-Forge.R-project.org')
+install_github('rCharts', 'ramnathv')
+install_github('shiny', 'rstudio')
+install_github('shiny-incubator', 'rstudio')
+install.packages('googleVis')
